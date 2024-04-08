082f38254ca95baffc339ec312753ed51953db73
LittleRabish <muyanh@umich.edu>
2024-04-07 21:01:20 -0400

    Add dark mode

---
+++ js/dark_mode.js
@@ -0,0 +1,16 @@
+document.getElementById("dark").addEventListener("click", function() {
+    document.body.classList.toggle("dark-mode");
+    document.querySelectorAll(
+        "h2, h3, .grid-travel, .grid-pets, .flex-recipes, .active, #nav .active a"
+    ).forEach(function(heading) {
+        heading.classList.toggle("dark-mode");
+    });
+    document.getElementById("dark").classList.toggle("dark-mode");
+    
+    if (document.body.className === "dark-mode") {
+        document.getElementById("dark").innerHTML = "Light Mode";
+    }
+    else {
+        document.getElementById("dark").innerHTML = "Dark Mode";
+    }
+})
